29449cfd029e8f55ba0ad7ce56b306f7d563be70
adrianhdez929 <adrianhdez929@gmail.com>
2022-05-26 13:09:41 -0400

    Initial board implementation and graph-like structure to manage game tables and moves

--- Domino/Boards.cs
+++ Domino/Boards.cs
@@ -1,5 +1,27 @@
-﻿namespace Domino.Boards;
+﻿using Domino.Moves;
+using Domino.Tokens;
+using Domino.Utils;
+
+namespace Domino.Boards;
 
 public abstract class BaseBoard {
+    // Table Like Structure
+    public Graph<BaseToken> Table { get; protected set; }
+    public BaseBoard(Graph<BaseToken> table) {
+        this.Table = table;
+    }
     // Move history
+    public abstract IEnumerable<BaseMove> Moves();
+}
+
+public class DominoBoard : BaseBoard {
+    List<BaseMove> moves = new List<BaseMove>();
+    int[] currentBoard;
+
+    public DominoBoard(Graph<BaseToken> table, DominoToken openToken) : base(table) {
+        this.currentBoard = openToken.Values().ToArray<int>();
+    }
+    public override IEnumerable<BaseMove> Moves() {
+        return this.moves;
+    }
 }
---
+++ Domino/Utils.cs
@@ -0,0 +1,76 @@
+
+
+namespace Domino.Utils;
+
+public class NullParentException : Exception {
+    public NullParentException(string message) : base(message) {}
+}
+
+public class NonNullNodeException : Exception {
+    public NonNullNodeException(string message) : base(message) {}
+}
+public class Graph<T> {
+    public HashSet<GraphLink<T>> Links { get; protected set; }
+    public HashSet<GraphNode<T>> Nodes { get; protected set; }
+    public GraphNode<T> Root { get; }
+
+    public Graph() {
+        this.Nodes = new HashSet<GraphNode<T>>();
+        this.Links = new HashSet<GraphLink<T>>();
+
+        this.Root = new GraphNode<T>();
+        Nodes.Add(this.Root);
+    }
+    public Graph(T root) {
+        this.Nodes = new HashSet<GraphNode<T>>();
+        this.Links = new HashSet<GraphLink<T>>();
+        
+        this.Root = new GraphNode<T>(root);
+        Nodes.Add(this.Root);
+    }
+
+    public void AddNodes(GraphNode<T> parent, int n) {
+        if (parent.Value == null)
+            throw new NullParentException("Parent node must not be null");
+        
+        for (int i = 0; i < n; i++) {
+            GraphNode<T> newNode = new GraphNode<T>();
+
+            this.Nodes.Add(newNode);
+            this.Links.Add(new GraphLink<T>(parent, newNode));
+        }
+    }
+
+    public IEnumerable<GraphLink<T>> GetLinksWithFreeNodes() {
+        return this.Links.Where(link => link.To.Value == null);
+    }
+    public IEnumerable<GraphNode<T>> GetFreeNodes() {
+        return this.Nodes.Where(node => node.Value == null);
+    }
+}
+
+public class GraphNode<T> {
+    public T? Value { get; private set; }
+
+    public GraphNode() {}
+
+    public GraphNode(T value) {
+        this.Value = value;
+    }
+
+    public void SetValue(T value) {
+        if (this.Value != null)
+            throw new NonNullNodeException("Node already contains a value");
+        this.Value = value;
+    }
+}
+
+public class GraphLink<T> {
+    public GraphNode<T> From { get; }
+    public GraphNode<T> To { get; }
+
+    public GraphLink(GraphNode<T> from, GraphNode<T> to) {
+        this.From = from;
+        this.To = to;
+    }
+}
