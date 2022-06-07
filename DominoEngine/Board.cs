2455918767c9e6a2353c253df98da7808523b0d0
asciber45 <alex.sierra@estudiantes.matcom.uh.cu>
2022-06-06 20:09:39 -0400

    Board IEnumerable

--- DominoEngine/Board.cs
+++ DominoEngine/Board.cs
@@ -1,37 +1,193 @@
-﻿namespace DominoEngine;
+﻿using System.Collections;
 
-public abstract class DominoBoard<T> : IBoard<DominoPiece<T>> where T : ILinkerable
+namespace DominoEngine;
+
+public class Node<T>
 {
-    IList<Stack<DominoPiece<T>>> branches = new List<Stack<DominoPiece<T>>>();
-    protected DominoBoard(DominoPiece<T> init, Rules rules) {}
+    private T _toParent;
+    private T _toChild;
+    private int _turn;
+    private Node<T>? _parent;
+    public Node<T>? Child = null;
 
-    public void NextAction(IPlayer<DominoPiece<T>> player, IBoard<DominoPiece<T>> state)
+    public Node(T toParent, T toChild, int turn, Node<T>? parent)
     {
-        player.Play(this);
+        _toParent = toParent;
+        _toChild = toChild;
+        _turn = turn;
+        _parent = parent;
     }
 
-    public IList<DominoPiece<T>> PossiblesActions()
-    {
-        IList<DominoPiece<T>> heads = new List<DominoPiece<T>>();
+    public T ToParent => _toParent;
+    public T ToChild => _toChild;
+    public int Turn => _turn;
 
-        foreach (var branch in branches)
+    public Node<T>? Parent { get => _parent; set => _parent = value; }
+}
+
+public record Ficha<T>(T Head, T Tail);
+
+public class StartNode<T> : Node<T>
+{
+    public StartNode(T toParent, T toChild) : base(toParent, toChild, 0, null) { }
+}
+
+public class Board<T> : IEnumerable<(int turn, bool right, T toParent, T toChild)>
+{
+
+    public StartNode<T> Salida; //rename	
+    private Node<T> _left;
+    private Node<T> _right;
+
+    public Node<T> Left => _left;
+    public Node<T> Right => _right;
+
+    public Board(Ficha<T> salida)
+    { //rename
+        Salida = new StartNode<T>(salida.Head, salida.Tail);
+        _left = Salida;
+        _right = Salida;
+    }
+
+    public bool AddLeft(T matching, T other, int turn)
+    {
+        var inNode = new Node<T>(matching, other, turn, Left);
+        if (Left.Equals(Salida))
         {
-            heads.Add(branch.Peek());
+            Left.Parent = inNode;
+            _left = Left.Parent;
+            return true;
         }
 
-        return heads;
+        Left.Child = inNode;
+        _left = Left.Child;
+        return true;
     }
 
-    public Stack<DominoPiece<T>> FindPiece(DominoPiece<T> piece)
+    public bool AddRight(T matching, T other, int turn)
     {
-        foreach (var branch in branches)
+        Right.Child = new Node<T>(matching, other, turn, Right);
+        _right = Right.Child;
+        return true;
+    }
+
+    public IEnumerator<(int turn, bool right, T toParent, T toChild)> GetEnumerator()
+    {
+        return Photo().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    private IEnumerable<(int turn, bool right, T toParent, T toChild)> Photo()
+    {
+        yield return (0, true, Salida.ToParent, Salida.ToChild);
+        var curLeft = Salida.Parent;
+        var curRight = Salida.Child;
+        while (!(curLeft is null && curRight is null))
         {
-            foreach (var item in branch)
+            if (curLeft is null)
             {
-                if (item.Equals(piece)) return branch;
+                var turn = curRight!.Turn;
+                var toParent = curRight.ToParent;
+                var toChild = curRight.ToChild;
+                curRight = curRight.Child;
+                yield return (turn, true, toParent, toChild);
+            }
+            else if (curRight is null)
+            {
+                var turn = curLeft!.Turn;
+                var toParent = curLeft.ToParent;
+                var toChild = curLeft.ToChild;
+                curLeft = curLeft.Child;
+                yield return (turn, false, toParent, toChild);
+            }
+            else if (curLeft.Turn < curRight.Turn)
+            {
+                var turn = curLeft!.Turn;
+                var toParent = curLeft.ToParent;
+                var toChild = curLeft.ToChild;
+                curLeft = curLeft.Child;
+                yield return (turn, false, toParent, toChild);
+            }
+            else
+            {
+                var turn = curRight!.Turn;
+                var toParent = curRight.ToParent;
+                var toChild = curRight.ToChild;
+                curRight = curRight.Child;
+                yield return (turn, true, toParent, toChild);
             }
         }
-
-        return null!;
     }
+}
+
+public record Move<T>(T Head, Ficha<T> ficha, bool rigth, Board<T> board);
+
+public class Hand<T> : IList<Ficha<T>>, ICloneable<Hand<T>>
+{
+    List<Ficha<T>> fichas;
+    public Hand(List<Ficha<T>> Fichas = null!)
+    {
+        fichas = (Fichas is null) ? new List<Ficha<T>>() : Fichas;
+    }
+
+    public Ficha<T> this[int index] { get => fichas[index]; set => fichas[index] = value; }
+
+    public int Count => fichas.Count;
+
+    public bool IsReadOnly => throw new NotImplementedException();
+
+    public void Add(Ficha<T> item) => fichas.Add(item);
+
+    public void Clear() => fichas.Clear();
+
+    public Hand<T> Clone()
+    {
+        List<Ficha<T>> new_list = new List<Ficha<T>>();
+
+        foreach (var item in fichas) new_list.Add(item);
+
+        return new Hand<T>(new_list);
+    }
+
+    public bool Contains(Ficha<T> item) => fichas.Contains(item);
+
+    public void CopyTo(Ficha<T>[] array, int arrayIndex)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<Ficha<T>> GetEnumerator()
+    {
+        return fichas.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public int IndexOf(Ficha<T> item) => fichas.IndexOf(item);
+
+    public void Insert(int index, Ficha<T> item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Remove(Ficha<T> item) => fichas.Remove(item);
+
+    public void RemoveAt(int index) => fichas.RemoveAt(index);
+
+    object ICloneable.Clone()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public abstract class Player<T>
+{
+
 }
