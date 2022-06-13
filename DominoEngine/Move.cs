d4c74cad7ab329b9db5135b48546d7c8ad3a0917
Omar Rivero <omarrivgw@gmail.com>
2022-06-13 03:18:10 -0400

    Juez terminado. Queda ver que hacemos con el juego.

--- DominoEngine/Move.cs
+++ DominoEngine/Move.cs
@@ -1,3 +1,5 @@
 namespace DominoEngine;
 
-public record Move<T>(int PlayerId, bool Check = true, int Turn = -2, T? Head = default, T? Tail = default);
+public record Move<T>(int PlayerId, bool Check = true, int Turn = -2, T? Head = default, T? Tail = default) {
+	public Ficha<T?> Ficha => new Ficha<T?>(Head, Tail);
+}
--- DominoEngine/Partida.cs
+++ DominoEngine/Partida.cs
@@ -1,18 +1,18 @@
 ﻿using DominoEngine;
 
 public class Partida<T> {
-	private readonly Board<T> _board = new Board<T>();
-	private readonly Dictionary<Player<T>, Hand<T>> _hands;
+	private readonly Board<T> _board = new();
+	private readonly Dictionary<Player<T>, Hand<T>> _hands = new();
 	private readonly List<Player<T>> _players;
 
-	public Partida(Dictionary<Player<T>, Hand<T>> hands) {
-		_hands = hands;
-		_players = hands.Keys.ToList();
+	public Partida(List<Player<T>> players) {
+		_players = players;
 	}
 
 	internal void AddMove(Move<T> move) => _board.Add(move);
 
-	internal bool InHand(Player<T> player, Ficha<T> ficha) => _hands.ContainsKey(player) && _hands[player].Contains(ficha);
+	internal bool InHand(Player<T> player, Ficha<T> ficha) =>
+		_hands.ContainsKey(player) && _hands[player].Contains(ficha);
 
 	internal bool RemoveFromHand(Player<T> player, Ficha<T> ficha) =>
 		_hands.ContainsKey(player) && _hands[player].Remove(ficha);
@@ -20,6 +20,8 @@ public class Partida<T> {
 	internal IEnumerable<Ficha<T>> Hand(Player<T> player) => _hands[player].Clone();
 
 	internal int PlayerId(Player<T> player) => _players.IndexOf(player);
-	
-	internal IEnumerable<Move<T>> Board => _board;
+
+	internal IEnumerable<Move<T>> Board => _board.ToArray();
+
+	internal void SetHand(Player<T> player, Hand<T> hand) => _hands[player] = hand.Clone();
 }
