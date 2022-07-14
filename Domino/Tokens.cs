186d724a8bb3e2881cae6f640e1f4bb16b3d6104
adrianhdez929 <adrianhdez929@gmail.com>
2022-07-14 17:01:53 -0400

    Added token generation independency to each token implementation

--- Domino/Tokens.cs
+++ Domino/Tokens.cs
@@ -1,26 +1,72 @@
 namespace Domino.Tokens;
 
-public class DominoToken {
+/// <summary>
+///     Base Domino Token implementation
+/// </summary>
+public class DominoToken : ITokenGenerator {
     public int Left { get; }
     public int Right { get; }
+    public bool IsDouble {
+        get {
+            return this.Left == this.Right;
+        }
+    }
 
+    public DominoToken() {
+        this.Left = -1;
+        this.Right = -1;
+    }
+
+    /// <param name="left">Left value of a given token</param>
+    /// <param name="right">Right value of a given token</param>
+    /// <remarks>
+    ///     There's no such thing as side concept for now, maybe this could change 
+    ///     to a collection in the future
+    /// </remarks>
     public DominoToken(int left, int right) {
         this.Left = left;
         this.Right = right;
     }
+
+    /// <summary>
+    ///     Numerical concept related to a token, where can be assigned an integer
+    ///     to a token for comparation, filter, etc
+    /// </summary>
     public virtual int Value() {
         return this.Left + this.Right;
     }
 
     public override string ToString()
     {
-        return $"({Left} | {Right}) : {this.Value()}";
+        return $"({Left} | {Right})";
+    }
+
+    public virtual string Represent() {
+        return "Domino Token";
+    }
+
+    public virtual IList<DominoToken> GenerateTokens(int tokenValues) { 
+        List<int[]> tokenValuesList = new List<int[]>();
+        List<DominoToken> tokens = new List<DominoToken>();
+
+        Utils.Utils.GenerateTokenValues(tokenValues, tokenValuesList);
+
+        foreach(int[] values in tokenValuesList) {
+            tokens.Add(new DominoToken(values[0], values[1]));
+        }
+
+        return tokens;
     }
 }
 
 public class SixUnvaluableDominoToken : DominoToken {
+    public SixUnvaluableDominoToken() : base() {}
     public SixUnvaluableDominoToken(int left, int right) : base(left, right) {}
 
+    /// <summary>
+    ///     Token value implementation where the number 6 doesn't represent a value
+    ///     to compute for the total
+    /// </summary>
     public override int Value()
     {
         int value = 0;
@@ -32,31 +78,62 @@ public class SixUnvaluableDominoToken : DominoToken {
 
         return value;
     }
-}
 
-public class DoubledValueDominoToken : DominoToken {
-    public DoubledValueDominoToken(int left, int right) : base(left, right) {}
-    public override int Value()
+    public override string Represent()
     {
-        return  2 * (this.Left + this.Right);
+        return $"Six Unvaluable {base.Represent()}";
     }
-}
-
-public interface ITokenGenerator<TToken> {
-    public IList<TToken> GenerateTokens(int tokenValues);
-}
-
-public class DominoTokenGenerator : ITokenGenerator<DominoToken> {
-    public IList<DominoToken> GenerateTokens(int tokenValues) {
+    
+    public override IList<DominoToken> GenerateTokens(int tokenValues) {
         List<int[]> tokenValuesList = new List<int[]>();
         List<DominoToken> tokens = new List<DominoToken>();
 
         Utils.Utils.GenerateTokenValues(tokenValues, tokenValuesList);
-        
+
         foreach(int[] values in tokenValuesList) {
-            tokens.Add(new DominoToken(values[0], values[1]));
+            tokens.Add(new SixUnvaluableDominoToken(values[0], values[1]));
         }
 
         return tokens;
     }
+}
+
+public class DoubledValueDominoToken : DominoToken {
+    public DoubledValueDominoToken() : base() {}    
+    public DoubledValueDominoToken(int left, int right) : base(left, right) {}
+
+    /// <summary>
+    ///     Token value implementation where the basic value implementation is doubled
+    /// </summary>
+    public override int Value()
+    {
+        return  2 * base.Value();
+    }
+
+    public override string Represent()
+    {
+        return $"Doubled Value {base.Represent()}";
+    }
+
+    public override IList<DominoToken> GenerateTokens(int tokenValues)
+    {
+        List<int[]> tokenValuesList = new List<int[]>();
+        List<DominoToken> tokens = new List<DominoToken>();
+
+        Utils.Utils.GenerateTokenValues(tokenValues, tokenValuesList);
+
+        foreach(int[] values in tokenValuesList) {
+            tokens.Add(new DoubledValueDominoToken(values[0], values[1]));
+        }
+
+        return tokens;
+    }
+}
+
+/// <summary>
+///     Token generator class. This creates a collection of token objects from
+///     a given maxNumber. E.g: 9-based Domino, 7-based Domino
+/// </summary>
+public interface ITokenGenerator {
+    public IList<DominoToken> GenerateTokens(int tokenValues);
 }
