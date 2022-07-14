945b1efc78054656becb980c2b83f9b835af5f2a
adrianhdez929 <adrianhdez929@gmail.com>
2022-07-14 17:02:08 -0400

    Fixed utils

--- Domino/Utils.cs
+++ Domino/Utils.cs
@@ -1,6 +1,10 @@
 namespace Domino.Utils;
 
 public static class Utils {
+    /// <summary>
+    ///     Given a number representing the game signature e.g: 9 for 9-based domino, generates
+    ///     a collection of all possible pairs of values for that game signature
+    /// </summary>
     public static void GenerateTokenValues(int n, List<int[]> collection) {
         for (int i = 0; i <= n; i++) {
             for (int j = i; j <= n; j++) {
