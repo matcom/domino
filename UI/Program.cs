301d0dda5d9064609b12a47396d793bc79ac3513
adrianhdez929 <adrianhdez929@gmail.com>
2022-08-29 23:38:22 -0400

    Adding modularity to ui project

--- UI/Program.cs
+++ UI/Program.cs
@@ -3,155 +3,193 @@ using Domino.Tokens;
 using Domino.Players;
 using Domino.Referee;
 
-System.Console.WriteLine("Welcome to this Domino Game");
-System.Console.WriteLine("To play you must select this relevant game options from a list:");
-System.Console.WriteLine();
+namespace UI;
 
-string[] options = new string[] {
-    "- Token Amount. Ex: Double 9",
-    "- Tokens in Starting Hand per Player",
-    "- Amount of players",
-    "- Player strategies, as well as play against them",
-    "- Win Condition for game termination",
-    "- How to select a Winner",
-    "- Token Value implementation"
-};
-
-foreach(string option in options)
-    System.Console.WriteLine(option);
-
-System.Console.WriteLine();
-System.Console.WriteLine("To continue press any key...");
-
-Console.ReadLine();
-Console.Clear();
-
-
-System.Console.WriteLine("Enter player amount: ");
-System.Console.WriteLine("Default: 4 players");
-System.Console.WriteLine();
-
-string? input = Console.ReadLine();
-int playerCount = int.Parse(input! == "" ? "4" : input!);
-
-Console.Clear();
-
-System.Console.WriteLine("Enter max value amount: ");
-System.Console.WriteLine("Default: Double 7");
-System.Console.WriteLine();
-
-input = Console.ReadLine();
-int tokenValue = int.Parse(input! == "" ? "7" : input!);
-
-Console.Clear();
-
-int maxTokensPerPlayer = (tokenValue * (tokenValue + 1)) / (2 * playerCount);
-
-System.Console.WriteLine("Enter player tokens amount: ");
-System.Console.WriteLine("Default: 7");
-System.Console.WriteLine();
-System.Console.WriteLine("NOTE: This is for standard 4 players");
-System.Console.WriteLine($"Otherwise this amount must be less or equal than {maxTokensPerPlayer}");
-System.Console.WriteLine();
-
-input = Console.ReadLine();
-int tokensPerPlayer = int.Parse(input! == "" ? "7" : input!);
-
-Console.Clear();
-
-
-DominoPlayer[] players = new DominoPlayer[playerCount];
-IWinCondition win;
-IWinner winner;
-DominoToken token;
-
-for (int i = 0; i < playerCount; i++) {
-    Console.Clear();
-    System.Console.WriteLine($"There are already {i} players");
+static void Main() {
+    System.Console.WriteLine("Welcome to this Domino Game");
+    System.Console.WriteLine("To play you must select this relevant game options from a list:");
     System.Console.WriteLine();
-    System.Console.WriteLine("Select a player to add: ");
 
-    DominoPlayer[] dominoPlayers = new DominoPlayer[]{
-        new HumanDominoPlayer($"Player {i + 1}"),
-        new RandomDominoPlayer($"Player {i + 1}"),
-        new GreedyDominoPlayer($"Player {i + 1}"),
-        new DataDominoPlayer($"Player {i + 1}", tokenValue)
+    string[] options = new string[] {
+        "- Token Amount. Ex: Double 9",
+        "- Tokens in Starting Hand per Player",
+        "- Amount of players",
+        "- Player strategies, as well as play against them",
+        "- Win Condition for game termination",
+        "- How to select a Winner",
+        "- Token Value implementation"
     };
 
-    System.Console.WriteLine($"Default: {dominoPlayers[0]}");
+    foreach(string option in options)
+        System.Console.WriteLine(option);
+
+    System.Console.WriteLine();
+    System.Console.WriteLine("To continue press any key...");
+
+    Console.ReadLine();
+    Console.Clear();
+
+    SetPlayerCount();
+    SetValueAmount();
+    SetPlayers();
+    SetWinCondition();
+    SetToken();
+    PrepareGame();
+}
+
+
+// Logic to allow user to enter the player amount
+static void SetPlayerCount() {
+    System.Console.WriteLine("Enter player amount: ");
+    System.Console.WriteLine("Default: 4 players");
     System.Console.WriteLine();
 
-    for (int j = 0; j < dominoPlayers.Length; j++) {
-        System.Console.WriteLine($"{j} - {dominoPlayers[j]}");
+    string? input = Console.ReadLine();
+    int playerCount = int.Parse(input! == "" ? "4" : input!);
+
+    Console.Clear();
+}
+
+// Logic to allow user to enter game type by token value
+static void SetValueAmount() {
+    System.Console.WriteLine("Enter max value amount: ");
+    System.Console.WriteLine("Default: Double 7");
+    System.Console.WriteLine();
+
+    string? input = Console.ReadLine();
+    int tokenValue = int.Parse(input! == "" ? "7" : input!);
+
+    Console.Clear();
+}
+
+// Logic to allow user to enter player types based on previosly selected amount
+static void SetPlayers() {
+    int maxTokensPerPlayer = (tokenValue * (tokenValue + 1)) / (2 * playerCount);
+
+    System.Console.WriteLine("Enter player tokens amount: ");
+    System.Console.WriteLine("Default: 7");
+    System.Console.WriteLine();
+    System.Console.WriteLine("NOTE: This is for standard 4 players");
+    System.Console.WriteLine($"Otherwise this amount must be less or equal than {maxTokensPerPlayer}");
+    System.Console.WriteLine();
+
+    string? input = Console.ReadLine();
+    int tokensPerPlayer = int.Parse(input! == "" ? "7" : input!);
+
+    Console.Clear();
+
+
+    DominoPlayer[] players = new DominoPlayer[playerCount];
+    IWinCondition win;
+    IWinner winner;
+    DominoToken token;
+
+    for (int i = 0; i < playerCount; i++) {
+        Console.Clear();
+        System.Console.WriteLine($"There are already {i} players");
+        System.Console.WriteLine();
+        System.Console.WriteLine("Select a player to add: ");
+
+        DominoPlayer[] dominoPlayers = new DominoPlayer[]{
+            new HumanDominoPlayer($"Player {i + 1}"),
+            new RandomDominoPlayer($"Player {i + 1}"),
+            new GreedyDominoPlayer($"Player {i + 1}"),
+            new DataDominoPlayer($"Player {i + 1}", tokenValue)
+        };
+
+        System.Console.WriteLine($"Default: {dominoPlayers[0]}");
+        System.Console.WriteLine();
+
+        for (int j = 0; j < dominoPlayers.Length; j++) {
+            System.Console.WriteLine($"{j} - {dominoPlayers[j]}");
+        }
+
+        input = Console.ReadLine();
+        int playerIndex = int.Parse(input! == "" ? "0" : input!);
+
+        players[i] = dominoPlayers[playerIndex];
+    }
+
+    Console.Clear();
+}
+
+// Logic to allow user to select a win condition for the game
+static void SetWinCondition() {
+    System.Console.WriteLine("Select a Win Condition: ");
+
+    IWinCondition[] winConditions = new IWinCondition[]{
+        new StandardWinCondition(),
+        new FourRoundsWinCondition()
+    };
+
+    System.Console.WriteLine($"Default: {winConditions[0]}");
+    System.Console.WriteLine();
+
+    for (int j = 0; j < winConditions.Length; j++) {
+        System.Console.WriteLine($"{j} - {winConditions[j]}");
     }
 
     input = Console.ReadLine();
-    int playerIndex = int.Parse(input! == "" ? "0" : input!);
+    int winIndex = int.Parse(input! == "" ? "0" : input!);
 
-    players[i] = dominoPlayers[playerIndex];
+    win = winConditions[winIndex];
+
+    Console.Clear();
 }
 
-Console.Clear();
-System.Console.WriteLine("Select a Win Condition: ");
+// Logic to allow user to set a win condition for the game
+static void SetWinCondition() {
+    System.Console.WriteLine("Select how to choose Winner: ");
 
-IWinCondition[] winConditions = new IWinCondition[]{
-    new StandardWinCondition(),
-    new FourRoundsWinCondition()
-};
+    IWinner[] winners = new IWinner[]{
+        new MinValueWinner(),
+        new MoreTokensWinner()
+    };
 
-System.Console.WriteLine($"Default: {winConditions[0]}");
-System.Console.WriteLine();
+    System.Console.WriteLine($"Default: {winners[0]}");
+    System.Console.WriteLine();
 
-for (int j = 0; j < winConditions.Length; j++) {
-    System.Console.WriteLine($"{j} - {winConditions[j]}");
+    for (int j = 0; j < winners.Length; j++) {
+        System.Console.WriteLine($"{j} - {winners[j]}");
+    }
+
+    input = Console.ReadLine();
+    int winnerIndex = int.Parse(input! == "" ? "0" : input!);
+
+    winner = winners[winnerIndex];
+
+    Console.Clear();
 }
 
-input = Console.ReadLine();
-int winIndex = int.Parse(input! == "" ? "0" : input!);
+// Logic to allow user to select the token value calculation
+static void SetToken() {
 
-win = winConditions[winIndex];
+    System.Console.WriteLine("Select a Token: ");
 
-Console.Clear();
-System.Console.WriteLine("Select how to choose Winner: ");
+    DominoToken[] tokens = new DominoToken[]{
+        new DominoToken(),
+        new SixUnvaluableDominoToken(),
+        new DoubledValueDominoToken()
+    };
 
-IWinner[] winners = new IWinner[]{
-    new MinValueWinner(),
-    new MoreTokensWinner()
-};
+    System.Console.WriteLine($"Default: {tokens[0]}");
+    System.Console.WriteLine();
 
-System.Console.WriteLine($"Default: {winners[0]}");
-System.Console.WriteLine();
+    for (int j = 0; j < tokens.Length; j++) {
+        System.Console.WriteLine($"{j} - {tokens[j]}");
+    }
 
-for (int j = 0; j < winners.Length; j++) {
-    System.Console.WriteLine($"{j} - {winners[j]}");
+    input = Console.ReadLine();
+    int tokenIndex = int.Parse(input! == "" ? "0" : input!);
+    token = tokens[tokenIndex];
+
+    Console.Clear();
 }
 
-input = Console.ReadLine();
-int winnerIndex = int.Parse(input! == "" ? "0" : input!);
+// Just wrapping the instantiation and execution of the game instance
+static void PrepareGame() {
+    DominoGame game = new DominoGame(
+        tokenValue, tokensPerPlayer, token, players, winner, win);
 
-winner = winners[winnerIndex];
-
-Console.Clear();
-System.Console.WriteLine("Select a Token: ");
-
-DominoToken[] tokens = new DominoToken[]{
-    new DominoToken(),
-    new SixUnvaluableDominoToken(),
-    new DoubledValueDominoToken()
-};
-
-System.Console.WriteLine($"Default: {tokens[0]}");
-System.Console.WriteLine();
-
-for (int j = 0; j < tokens.Length; j++) {
-    System.Console.WriteLine($"{j} - {tokens[j]}");
+    game.Result();
 }
-
-input = Console.ReadLine();
-int tokenIndex = int.Parse(input! == "" ? "0" : input!);
-token = tokens[tokenIndex];
-
-DominoGame game = new DominoGame(
-    tokenValue, tokensPerPlayer, token, players, winner, win);
-
-game.Result();
